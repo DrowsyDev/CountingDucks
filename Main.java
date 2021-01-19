7f8913f323d5b4a3eb31cacbdb01a56fe23722aa
Roberto Santoyo <91098230@g.mvusd.net>
2021-01-19 20:00:41 +0000

    Added a functioning second button and a label

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.event.*; //this enables the events
+import java.awt.*;
 
 public class Main
 {
@@ -9,6 +10,10 @@ public class Main
     frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     //YOU MUST HAVE THIS LINE
     frame1.setSize(500,500);
+    frame1.setVisible(true);
+
+    //--------Panel ro old buttons------
+    JPanel panel1 = new JPanel(new GridLayout(2,2, 20,20));
 
     //---------------Button 1-----------------------
     JButton button1 = new JButton("Click me!");
@@ -21,9 +26,23 @@ public class Main
         }
     }); //end the ActionListener 
 
-    frame1.add(button1);
+    //--------------Button 2--------------
+    JButton button2 = new JButton ("No, click me");
+    button2.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        System.out.println("Thanks, pal!");
+      }
+    });
+    //--------Label 1------------
+    JLabel label1 = new JLabel("Count the ducks");
+
+    panel1.add(button1);
+    panel1.add(button2);
+    panel1.add(label1);
     
+    frame1.getContentPane().add(panel1);
     
-    frame1.setVisible(true);
   }
 }
