f63212df1d29d081d14bcb4c1c8c7629a2d551a7
Roberto Santoyo <91098230@g.mvusd.net>
2021-01-19 19:32:52 +0000

    Added a functioning button

--- Main.java
+++ Main.java
@@ -9,7 +9,6 @@ public class Main
     frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     //YOU MUST HAVE THIS LINE
     frame1.setSize(500,500);
-    
 
     //---------------Button 1-----------------------
     JButton button1 = new JButton("Click me!");
